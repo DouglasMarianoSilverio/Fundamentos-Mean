01871265b7a70167f087560226384c2fe2ad1ed4
DouglasMarianoSilverio <douglas07@gmail.com>
2019-02-25 13:33:27 -0300

    configuração inicial do frontend em angular1

---
+++ angular1/gulpTasks/deps.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp')
+const uglify = require ('gulp-uglify')
+
+gulp.task('deps',['deps.js','deps.css','deps.fonts'])
+
+gulp.task('deps.js',()=>{
+     
+})
+
+gulp.task('deps.css',()=>{
+    
+})
+
+gulp.task('deps.fonts',()=>{
+    
+})
