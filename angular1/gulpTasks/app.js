fe7d83286e17f3cacfb5667ff4da18d44ce250db
DouglasMarianoSilverio <douglas07@gmail.com>
2019-02-27 10:32:35 -0300

    configuração de tasks do gulp

--- angular1/gulpTasks/app.js
+++ angular1/gulpTasks/app.js
@@ -1,9 +1,16 @@
 const gulp = require('gulp')
+const babel = require('gulp-babel')
+const uglify = require('gulp-uglify')
+const uglifycss = require('gulp-uglifycss')
+const concat = require('gulp-concat')
+const htmlmin = require('gulp-htmlmin')
 
 gulp.task('app',['app.html','app.css','app.js','app.assets'])
 
 gulp.task('app.html', () =>{
-    
+    return gulp.src('app/**/*.html')
+    .pipe(htmlmin({collapseWhitespace:true}))
+    .pipe(gulp.dest('public'))
 })
 
 
