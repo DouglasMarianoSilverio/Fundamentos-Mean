07d2be2a82c418aa94e12f541dd497511e7c2d11
DouglasMarianoSilverio <douglas07@gmail.com>
2019-03-07 13:39:01 -0300

    Componente de cabeçalho no conteudo

---
+++ angular1/app/common/components/contentHeader.js
@@ -0,0 +1,12 @@
+angular.module('primeiraApp').component('contentHeader',{
+    bindings:{
+        /*@ string e nao muda*/
+        name:'@',
+        small:'@',
+    },
+    template: `
+    <section class="content-header">
+    <h1>{{$ctrl.name}} <small>{{$ctrl.small}}</small> </h1>
+    </section>
+    `
+})
