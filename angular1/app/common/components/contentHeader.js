25b187e908ac869299c4e9631290a14f4a7e2688
DouglasMarianoSilverio <douglas07@gmail.com>
2019-03-11 13:27:52 -0300

    implementação de melhores praticas. implementação de Allow CORS no backend. implementação de codigo para consumir a api do billingSummary

--- angular1/app/common/components/contentHeader.js
+++ angular1/app/common/components/contentHeader.js
@@ -1,12 +1,16 @@
-angular.module('primeiraApp').component('contentHeader',{
-    bindings:{
-        /*@ string e nao muda*/
-        name:'@',
-        small:'@',
-    },
-    template: `
-    <section class="content-header">
-    <h1>{{$ctrl.name}} <small>{{$ctrl.small}}</small> </h1>
-    </section>
-    `
-})
+(function(){
+    angular.module('primeiraApp').component('contentHeader',{
+        bindings:{
+            /*@ string e nao muda*/
+            name:'@',
+            small:'@',
+        },
+        template: `
+        <section class="content-header">
+        <h1>{{$ctrl.name}} <small>{{$ctrl.small}}</small> </h1>
+        </section>
+        `
+    })
+})()
+
+
