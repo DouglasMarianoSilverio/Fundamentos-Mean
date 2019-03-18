a45bd3bfae16cbb0011abaa5dc1b51e1fceef372
DouglasMarianoSilverio <douglas07@gmail.com>
2019-03-18 12:54:43 -0300

    Lista de Ciclo de Pagamentos

--- angular1/app/billingCycle/billingCycleController.js
+++ angular1/app/billingCycle/billingCycleController.js
@@ -1,19 +1,32 @@
-(function(){
-angular.module('primeiraApp').controller('BillingCycleCtrl',[
-    '$http',
-    BillingCycleController
-])
+(function () {
+    angular.module('primeiraApp').controller('BillingCycleCtrl', [
+        '$http',
+        'msgs',
+        BillingCycleController
+    ])
 
-function BillingCycleController($http){
-     const vm = this
-     vm.create = function(){
-         const url = 'http://localhost:3003/api/billingCycles'
+    function BillingCycleController($http, msgs) {
+        const vm = this
+        const url = 'http://localhost:3003/api/billingCycles'
 
-         $http.post(url, vm.billingCycle).then(function(response){
-             vm.billingCycle={}
-             console.log('Sucesso!')
-         })
+        vm.refresh = function () {
+            $http.get(url).then(function(response){
+                 vm.billingCycle = {}
+                 vm.billingCycles = response.data
+            })
+        }
 
-     }
-}
+        vm.create = function () {
+
+            $http.post(url, vm.billingCycle).then(function (response) {
+                vm.refresh()
+                msgs.addSuccess('Operação realizada com sucesso!!')
+            }).catch(function (response) {
+                msgs.addError(response.data.errors)
+            })
+
+        }
+
+        vm.refresh()
+    }
 })()
